8897b3faf6d28c2a4f43045df188dfbafb1fc522
Jay-flow <kimpetro153@gmail.com>
2022-03-15 20:35:38 +0900

    Remove countries

--- src/main.go
+++ src/main.go
@@ -5,52 +5,36 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/fedesog/webdriver"
-	"github.com/joho/godotenv"
 	"github.com/tebeka/selenium"
 )
 
-var contries []string
 var session webdriver.Session
 
 func main() {
-	contries = []string{"Japan", "United Kingdom", "South Korea", "United States"}
-	videoCounts := 30
-	godotenv.Load("/Users/dooboolab/GoProjects/video-crawler/.env")
+	videoCounts := 100
 
 	session = setupWebDriver()
 
-	goToTheSignInPage()
-	signIn()
-
-	// fileName := createTheCSV()
-	// f, err := os.Create(fileName)
-	// checkErr(err)
-
 	f, w := createTheCSV()
 
-	for _, contry := range contries {
-		selectTheContry(contry)
+	goToTheShort()
 
-		time.Sleep(4 * time.Second)
-		session.Refresh()
+	for i := 1; i <= videoCounts; i++ {
+		currentURL := getCurrnetShortUrl()
+		fmt.Println(currentURL)
+
+		rawURL := strings.Split(currentURL, "/")
+		data := []string{rawURL[len(rawURL)-1], currentURL}
+
+		w.Write(data)
+		w.Flush()
+
 		time.Sleep(1 * time.Second)
-
-		goToTheShort()
-
-		for i := 1; i <= videoCounts; i++ {
-			currentURL := getCurrnetShortUrl()
-			fmt.Println(currentURL)
-			data := []string{currentURL, currentURL, contry}
-
-			w.Write(data)
-			w.Flush()
-
-			time.Sleep(1 * time.Second)
-			goToTheNextShort()
-		}
+		goToTheNextShort()
 	}
 
 	f.Close()
@@ -78,65 +62,6 @@ func setupWebDriver() webdriver.Session {
 	session.SetTimeoutsImplicitWait(3000)
 
 	return *session
-}
-
-func goToTheSignInPage() {
-	signInButton, err := session.FindElement(selenium.ByCSSSelector, "tp-yt-paper-button[aria-label='Sign in']")
-	checkErr(err)
-	signInButton.Click()
-}
-
-func signIn() {
-	inputEmail, err := session.FindElement(selenium.ByCSSSelector, "input[type='email']")
-	checkErr(err)
-
-	email := os.Getenv("USER_ID")
-	inputEmail.SendKeys(email)
-
-	nextButton, err := session.FindElement(selenium.ByCSSSelector, "#identifierNext")
-	checkErr(err)
-	nextButton.Click()
-
-	time.Sleep(2 * time.Second)
-	inputPassword, err := session.FindElement(selenium.ByCSSSelector, "input[type='password']")
-	checkErr(err)
-	password := os.Getenv("USER_PW")
-	inputPassword.SendKeys(password)
-
-	signInButton, err := session.FindElement(selenium.ByCSSSelector, "#passwordNext")
-	checkErr(err)
-	signInButton.Click()
-	time.Sleep(1 * time.Second)
-}
-
-func selectTheContry(country string) {
-	profileButton, err := session.FindElement(selenium.ByCSSSelector, "#avatar-btn")
-	checkErr(err)
-	profileButton.Click()
-
-	locationButton, err := session.FindElements(selenium.ByCSSSelector, "#endpoint > tp-yt-paper-item")
-	checkErr(err)
-	locationButton[6].Click()
-
-	locations, err := session.FindElements(selenium.ByCSSSelector, "#items #endpoint yt-formatted-string[id='label']")
-	checkErr(err)
-	index, isFound := findTheCuntryIndex(locations, country)
-	if isFound {
-		locations[index].Click()
-	}
-}
-
-func findTheCuntryIndex(locations []webdriver.WebElement, country string) (int, bool) {
-	for i, location := range locations {
-		text, err := location.Text()
-		checkErr(err)
-		fmt.Println(i, text)
-		if text == country {
-			return i, true
-		}
-	}
-
-	return -1, false
 }
 
 func goToTheShort() {
@@ -170,7 +95,7 @@ func createTheCSV() (*os.File, *csv.Writer) {
 
 	w := csv.NewWriter(f)
 
-	title := []string{"id", "url", "state"}
+	title := []string{"id", "url"}
 	w.Write(title)
 	w.Flush()
 
